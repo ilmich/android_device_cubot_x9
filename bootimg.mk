48af66c5622c1f92deb4f42ccf2068eaef37f42b
ilmich <ardutu@gmail.com>
2016-02-27 12:50:50 +0100

    Removed warning during build

--- bootimg.mk
+++ bootimg.mk
@@ -28,15 +28,15 @@ $(INSTALLED_KERNEL_TARGET).mtk: $(INSTALLED_KERNEL_TARGET).mtk.header
 	cat $(INSTALLED_KERNEL_TARGET) > $@
 	
 INSTALLED_RAMDISK_TARGET := $(BUILT_RAMDISK_TARGET)
-$(INSTALLED_RAMDISK_TARGET): $(MKBOOTFS) $(INTERNAL_RAMDISK_FILES) | $(MINIGZIP)
-	$(call pretty,"Target ram disk: $@")
-	cmp -s device/cubot/x9/rootdir/custom_init out/target/product/x9/root/init; \
-	RETVAL=$$?; \
-	if [ $$RETVAL -eq 1 ]; then \
-		mv out/target/product/x9/root/init out/target/product/x9/root/init2; \
-		cp device/cubot/x9/rootdir/custom_init out/target/product/x9/root/init; \
-	fi
-	$(hide) $(MKBOOTFS) $(TARGET_ROOT_OUT) | $(MINIGZIP) > $@
+#$(INSTALLED_RAMDISK_TARGET): $(MKBOOTFS) $(INTERNAL_RAMDISK_FILES) | $(MINIGZIP)
+#	$(call pretty,"Target ram disk: $@")
+#	cmp -s device/cubot/x9/rootdir/custom_init out/target/product/x9/root/init; \
+#	RETVAL=$$?; \
+#	if [ $$RETVAL -eq 1 ]; then \
+#		mv out/target/product/x9/root/init out/target/product/x9/root/init2; \
+#		cp device/cubot/x9/rootdir/custom_init out/target/product/x9/root/init; \
+#	fi
+#	$(hide) $(MKBOOTFS) $(TARGET_ROOT_OUT) | $(MINIGZIP) > $@
 
 $(INSTALLED_RAMDISK_TARGET).mtk.header: $(INSTALLED_RAMDISK_TARGET)
 	size=$$($(call get-file-size,$(INSTALLED_RAMDISK_TARGET))); \
